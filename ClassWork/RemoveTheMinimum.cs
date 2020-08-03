a160530a94287c26fc39e01fb404b848b2f1d127
zyxxyGerman <56539917+zyxxyGerman@users.noreply.github.com>
2020-08-03 17:43:59 +0300

    добавил решение 2-х задач

---
+++ ClassWork/RemoveTheMinimum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lessons.ClassWork
+{
+    public class Remover
+    {
+        public static List<int> RemoveSmallest(List<int> numbers)
+        {
+            if(numbers.Count > 0)
+            {
+                int min = numbers[0];
+                foreach (int b in numbers)
+                {
+                    if (b < min)
+                    {
+                        min = b;
+                    }
+                }
+                numbers.Remove(min);
+            }
+            return numbers;
+        }  
+    }
+}
---
+++ ClassWork/ReverseWords.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string ReverseWords(string str)
+    {
+        string[] words = str.Split(new char[] { ' ' });
+        for (int a = 0; a < words.Length; a++)
+        {
+            words[a] = new string(words[a].Reverse().ToArray());
+        }
+        return string.Join(" ", words);
+    }
+}
